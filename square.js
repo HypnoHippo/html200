42792f2b0ea263d2284865f60e1532b5d6ab2484
HypnoHippo <slechta.forrest@gmail.com>
2021-10-03 02:05:54 -0700

    Finished Challenge 1

---
+++ square.js
@@ -0,0 +1,18 @@
+function numberSquare() {
+  let input = prompt('Enter a number, get the square!');
+  let result = 0;
+
+  result = Number(input) * Number(input);
+
+  alert(result);
+}
+
+function bonus() {
+  let a = prompt('Enter a number');
+  let b = prompt('Enter a second number');
+  let result = 0;
+
+  result = (Number(a) * Number(b));
+
+  alert(result);
+}
